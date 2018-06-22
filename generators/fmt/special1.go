092fa9ea95e6d2b7fa52699159ec88d5d851957c
pbberlin <peter.buchmann@web.de>
2018-06-22 18:45:04 +0200

    seasonal questions of 2nd quarter month

---
+++ generators/fmt/special1.go
@@ -0,0 +1,236 @@
+package fmt
+
+import (
+	"github.com/zew/go-questionaire/qst"
+	"github.com/zew/go-questionaire/trl"
+)
+
+// Seasonal questions revolve around the month of the Quarter.
+//
+// 	1 of quarter: Business cycle & drivers: 		         Januar, April, Juli, October
+// 	2 of quarter: Inflation, drivers, central bank rates:    Februar, May, August, November
+// 	3 of quarter: Free special questoins:                    March, June, September, December
+func addSeasonal1(q *qst.QuestionaireT) {
+
+	if monthOfQuarter() != 1 && false {
+		return
+	}
+
+	p := q.AddPage()
+	p.Section = trl.S{"de": "Sonderfrage", "en": "Special"}
+	p.Label = trl.S{"de": "Prognosetreiber Wachstum", "en": "Growth drivers"}
+	p.Width = 80
+
+	{
+		gr := p.AddGroup()
+		gr.Cols = 9
+		gr.Label = trl.S{
+			"de": "1.",
+			"en": "1.",
+		}
+		gr.Desc = trl.S{
+			"de": "Punktprognose der Wachstumsrate des deutschen BIP",
+			"en": "Forecast growth rate German GDP",
+		}
+
+		{
+			inp := gr.AddInput()
+			inp.Type = "textblock"
+			inp.Desc = trl.S{
+				"de": " <br>\n Prognose Wachstum des BIP <b>je Quartal</b> <br>\n (real, saisonbereinigt, nicht annualisiert) <br>\n <br>\n ",
+				"en": " <br>\n Forecast <b>quarterly</b> GDP growth <br>\n(real, seasonally adjusted, non annualized) <br>\n <br>\n ",
+			}
+			inp.ColSpanLabel = 3
+		}
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "xquart1"
+			inp.MaxChars = 4
+			inp.Validator = "inRange20"
+			inp.Desc = trl.S{
+				"de": nextQ(-1) + " &nbsp; ",
+				"en": nextQ(-1) + " &nbsp; ",
+			}
+			inp.Suffix = trl.S{
+				"de": "%",
+				"en": "pct",
+			}
+			inp.HAlignLabel = qst.HRight
+
+		}
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "xquart2"
+			inp.MaxChars = 4
+			inp.Validator = "inRange20"
+			inp.Desc = trl.S{
+				"de": nextQ(0) + " &nbsp; ",
+				"en": nextQ(0) + " &nbsp; ",
+			}
+			inp.Suffix = trl.S{
+				"de": "%",
+				"en": "pct",
+			}
+			inp.HAlignLabel = qst.HRight
+		}
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "xquart3"
+			inp.MaxChars = 4
+			inp.Validator = "inRange20"
+			inp.Desc = trl.S{
+				"de": nextQ() + " &nbsp; ",
+				"en": nextQ() + " &nbsp; ",
+			}
+			inp.Suffix = trl.S{
+				"de": "%",
+				"en": "pct",
+			}
+			inp.HAlignLabel = qst.HRight
+
+		}
+
+		{
+			inp := gr.AddInput()
+			inp.Type = "textblock"
+			inp.Desc = trl.S{
+				"de": "Prognose Wachstum des BIP aufs&nbsp;<b>Jahr</b> <br>\n(real, saisonbereinigt)",
+				"en": "Forecast GDP growth per&nbsp;<b>year</b> <br>\n(real, seasonally adjusted)",
+			}
+			inp.ColSpanLabel = 3
+
+		}
+
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "xyear1"
+			inp.MaxChars = 4
+			inp.Validator = "inRange20"
+			inp.Desc = trl.S{
+				"de": nextY(-1) + " &nbsp; ",
+				"en": nextY(-1) + " &nbsp; ",
+			}
+			inp.Suffix = trl.S{
+				"de": "%",
+				"en": "pct",
+			}
+			inp.HAlignLabel = qst.HRight
+
+		}
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "xyear2"
+			inp.MaxChars = 4
+			inp.Validator = "inRange20"
+			inp.Desc = trl.S{
+				"de": nextY(0) + " &nbsp; ",
+				"en": nextY(0) + " &nbsp; ",
+			}
+			inp.Suffix = trl.S{
+				"de": "%",
+				"en": "pct",
+			}
+			inp.HAlignLabel = qst.HRight
+		}
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "xyear3"
+			inp.MaxChars = 4
+			inp.Validator = "inRange20"
+			inp.Desc = trl.S{
+				"de": nextY() + " &nbsp; ",
+				"en": nextY() + " &nbsp; ",
+			}
+			inp.Suffix = trl.S{
+				"de": "%",
+				"en": "pct",
+			}
+			inp.HAlignLabel = qst.HRight
+
+		}
+
+	}
+
+	// gr1
+	{
+		labels123Matrix := []trl.S{
+			{
+				"de": "Konjunkturdaten Deutschland",
+				"en": "Business cycle data Germany",
+			},
+			{
+				"de": "Exportmärkte",
+				"en": "Export markets",
+			},
+			{
+				"de": "Wechselkurse",
+				"en": "Exchange rates",
+			},
+			{
+				"de": "EZB-Geldpolitik",
+				"en": "ECB monetary policy",
+			},
+			{
+				"de": "US-Geldpolitik",
+				"en": "FED monetary policy",
+			},
+			{
+				"de": "Geopol. Ereignisse",
+				"en": "Geopolitical events",
+			},
+			{
+				"de": "Regierungsbildung Deutschland",
+				"en": "Government formation Germany",
+			},
+			{
+				"de": "Sonstige",
+				"en": "Other",
+			},
+		}
+		// iobc => impact on business cycle
+		names1stMatrix := []string{
+			"iobc_cycle_data_deu",
+			"iobc_exp_markets",
+			"iobc_exch_rates",
+			"iobc_mp_ecb",
+			"iobc_mp_fed",
+			"iobc_geopol",
+			"iobc_gvt_form_deu",
+			"iobc_other",
+		}
+		gr := p.AddRadioMatrixGroup(labelsStronglyPositiveStronglyNegativeInfluence(),
+			names1stMatrix, labels123Matrix, 2)
+		gr.Cols = 8 // necessary, otherwise no vspacers
+		gr.OddRowsColoring = true
+		gr.Label = trl.S{
+			"de": "2.",
+			"en": "2.",
+		}
+		gr.Desc = trl.S{
+			"de": "Haben Entwicklungen in den folgenden Bereichen Sie zu einer Revision Ihrer Konjunkturprognosen (ggü. Vormonat) für die deutsche Wirtschaft bewogen und wenn ja in welche Richtung?",
+			"en": "Which developments have lead you to change your assessment of the business cycle outlook for the German economy compared to the previous month",
+		}
+
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "other_cycle_infl"
+			inp.Desc = trl.S{
+				"de": "Wenn sonstige - welche ?",
+				"en": "If other - which?",
+			}
+			inp.MaxChars = 30
+			inp.ColSpanLabel = 2
+			inp.ColSpanControl = 6
+			inp.CSSControl = "input-smaller"
+		}
+
+	}
+
+}
---
+++ generators/fmt/special2.go
@@ -0,0 +1,250 @@
+package fmt
+
+import (
+	"github.com/zew/go-questionaire/qst"
+	"github.com/zew/go-questionaire/trl"
+)
+
+func addSeasonal2(q *qst.QuestionaireT) {
+
+	if monthOfQuarter() != 2 && false {
+		return
+	}
+
+	p := q.AddPage()
+	p.Section = trl.S{"de": "Sonderfrage", "en": "Special"}
+	p.Label = trl.S{"de": "Prognosetreiber Inflation + Geldpolitik", "en": "Inflation + monetary policy drivers"}
+	p.Width = 90
+
+	{
+		gr := p.AddGroup()
+		gr.Cols = 9
+		gr.Label = trl.S{
+			"de": "1.",
+			"en": "1.",
+		}
+		gr.Desc = trl.S{
+			"de": "Punktprognose der jährlichen Inflationsrate im Euroraum",
+			"en": "Forecast yearly inflation rate in the Euro area",
+		}
+
+		{
+			inp := gr.AddInput()
+			inp.Type = "textblock"
+			inp.Desc = trl.S{
+				"de": "Anstieg des HICP von Jan bis Dez; Erwartungswert",
+				"en": "HICP  increase from Jan to Dec; expected value",
+			}
+			inp.ColSpanLabel = 3
+		}
+
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "p1_y1"
+			inp.MaxChars = 4
+			inp.Validator = "inRange20"
+			inp.Desc = trl.S{
+				"de": nextY(0) + " &nbsp; ",
+				"en": nextY(0) + " &nbsp; ",
+			}
+			inp.Suffix = trl.S{
+				"de": "%",
+				"en": "pct",
+			}
+			inp.HAlignLabel = qst.HRight
+
+		}
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "p1_y2"
+			inp.MaxChars = 4
+			inp.Validator = "inRange20"
+			inp.Desc = trl.S{
+				"de": nextY(1) + " &nbsp; ",
+				"en": nextY(1) + " &nbsp; ",
+			}
+			inp.Suffix = trl.S{
+				"de": "%",
+				"en": "pct",
+			}
+			inp.HAlignLabel = qst.HRight
+		}
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "p1_y3"
+			inp.MaxChars = 4
+			inp.Validator = "inRange20"
+			inp.Desc = trl.S{
+				"de": nextY(2) + " &nbsp; ",
+				"en": nextY(2) + " &nbsp; ",
+			}
+			inp.Suffix = trl.S{
+				"de": "%",
+				"en": "pct",
+			}
+			inp.HAlignLabel = qst.HRight
+
+		}
+
+	}
+
+	// gr1
+	{
+		labels123Matrix := []trl.S{
+			{
+				"de": "Konjunkturdaten Euroraum",
+				"en": "Business cycle data Euro area",
+			},
+			{
+				"de": "Konjunkturdaten global",
+				"en": "Business cycle data globally",
+			},
+			{
+				"de": "Löhne Euroraum",
+				"en": "Wages Euro area",
+			},
+			{
+				"de": "Rohstoffpreise",
+				"en": "Raw material prices",
+			},
+			{
+				"de": "Wechselkurse",
+				"en": "Exchange rates",
+			},
+			{
+				"de": "EZB-Geldpolitik",
+				"en": "ECB monetary policy",
+			},
+		}
+		// ioi => impact on inflation
+		names1stMatrix := []string{
+			"ioi_cycle_data_ea",
+			"ioi_cycle_data_gl",
+			"ioi_wages_ea",
+			"ioi_rmp",
+			"ioi_exch_rates",
+			"ioi_mp_ecb",
+		}
+		gr := p.AddRadioMatrixGroup(labelsStronglyPositiveStronglyNegativeInfluence(),
+			names1stMatrix, labels123Matrix, 2)
+		gr.Cols = 8 // necessary, otherwise no vspacers
+		gr.OddRowsColoring = true
+		gr.Label = trl.S{
+			"de": "2.",
+			"en": "2.",
+		}
+		gr.Desc = trl.S{
+			"de": "Haben Entwicklungen in den folgenden Bereichen Sie zu einer Revision Ihrer Inflationsprognosen (ggü. Vormonat) für den Euroraum bewogen und wenn ja in welche Richtung?",
+			"en": "Which developments have lead you to change your assessment of the inflation outlook for the Euro are compared to the previous month",
+		}
+	}
+
+	{
+		gr := p.AddGroup()
+		gr.Label = trl.S{"de": "3.", "en": "3."}
+		gr.Desc = trl.S{
+			"de": "Den Hauptrefinanzierungssatz der EZB (am 01.02.2018: 0,0%) erwarte ich auf Sicht von ",
+			"en": "I expect the main refinance rate of the ECB (01.02.2018: 0,0%) in ",
+		}
+		gr.Cols = 100
+
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "i_ez_06_low"
+			inp.MaxChars = 4
+			inp.Validator = "inRange20"
+
+			inp.ColSpanLabel = 15
+			inp.CSSLabel = "vert-wider"
+			inp.ColSpanControl = 9
+			inp.Desc = trl.S{
+				"de": " &nbsp; 6 Monaten zwischen",
+				"en": " &nbsp; 6 months between",
+			}
+			inp.Suffix = trl.S{"de": "%", "en": "pct"}
+			inp.HAlignLabel = qst.HLeft
+			inp.HAlignControl = qst.HLeft
+		}
+
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "i_ez_06_high"
+			inp.MaxChars = 4
+			inp.Validator = "inRange20"
+
+			inp.ColSpanLabel = 3
+			inp.ColSpanControl = 73
+			inp.Desc = trl.S{
+				"de": "und",
+				"en": "and",
+			}
+			inp.Suffix = trl.S{"de": "%", "en": "pct"}
+			inp.HAlignLabel = qst.HLeft
+			inp.HAlignControl = qst.HLeft
+		}
+
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "i_ez_24_low"
+			inp.MaxChars = 4
+			inp.Validator = "inRange20"
+
+			inp.ColSpanLabel = 15
+			inp.CSSLabel = "vert-wider"
+			inp.ColSpanControl = 9
+			inp.Desc = trl.S{
+				"de": " 24 Monaten zwischen",
+				"en": " 24 months between",
+			}
+			inp.Suffix = trl.S{"de": "%", "en": "pct"}
+			inp.HAlignLabel = qst.HLeft
+			inp.HAlignControl = qst.HLeft
+		}
+
+		{
+			inp := gr.AddInput()
+			inp.Type = "text"
+			inp.Name = "i_ez_24_high"
+			inp.MaxChars = 4
+			inp.Validator = "inRange20"
+
+			inp.ColSpanLabel = 3
+			inp.ColSpanControl = 73
+			inp.Desc = trl.S{
+				"de": "und",
+				"en": "and",
+			}
+			inp.Suffix = trl.S{"de": "%", "en": "pct"}
+			inp.HAlignLabel = qst.HLeft
+			inp.HAlignControl = qst.HLeft
+		}
+
+		{
+			inp := gr.AddInput()
+			inp.Type = "textblock"
+			inp.ColSpanLabel = 15
+			inp.Desc = trl.S{
+				"de": " &nbsp;",
+				"en": " &nbsp;",
+			}
+		}
+		{
+			inp := gr.AddInput()
+			inp.Type = "textblock"
+			inp.ColSpanLabel = 85
+			inp.Desc = trl.S{
+				"de": " [zentrales 90% Konfidenzintervall]",
+				"en": " [zentrales 90% Konfidenzintervall]",
+			}
+			inp.CSSLabel = "textblock-smaller"
+		}
+
+	}
+
+}

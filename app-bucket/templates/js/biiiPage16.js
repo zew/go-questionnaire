c7bfeb8192e86b390fdb174c71a563256d9d08c4
Peter Buchmann <peter.buchmann@zew.de>
2022-05-20 12:06:34 +0200

    BIII q33 with JS validation

--- app-bucket/templates/js/biiiPage16.js
+++ app-bucket/templates/js/biiiPage16.js
@@ -1,5 +1,9 @@
+
+let evtCounter = 0;
+
 function countToMax(event) {
 
+    evtCounter++;
 
     let q33aVal1 = 0, q33aVal2 = 0, q33aVal3 = 0;
     let q33bVal1 = 0, q33bVal2 = 0, q33bVal3 = 0;
@@ -46,19 +50,40 @@ function countToMax(event) {
             }
 
         }
-
-        if (q33aVal1 > 3 || q33aVal2 > 3 || q33aVal3 > 3) {
-            console.log(`q33a: Maximal drei je Spalte \n  col1 ${q33aVal1} col2 ${q33aVal2}  col3 ${q33aVal3}`);
-        }
-
-        if (q33bVal1 > 3 || q33bVal2 > 3 || q33bVal3 > 3) {
-            console.log(`q33b: Maximal drei je Spalte \n col1 ${q33bVal1} col2 ${q33bVal2}  col3 ${q33bVal3}`);
-        }
-
-        console.log(`q33a col1 ${q33aVal1} col2 ${q33aVal2}  col3 ${q33aVal3}  \nq33b col1 ${q33bVal1} col2 ${q33bVal2}  col3 ${q33bVal3}`);
-
-
     }
+
+    let invalid = false;
+
+    let tgtName = "undef";
+    if (event && event.target) {
+        tgtName = event.target.name;        
+    }    
+
+    if (q33aVal1 > 3 || q33aVal2 > 3 || q33aVal3 > 3) {
+        console.log(`q33a: Maximal drei je Spalte \n  col1 ${q33aVal1} col2 ${q33aVal2}  col3 ${q33aVal3} - ${tgtName} ${evtCounter}`);
+        invalid = true;
+    }
+
+    if (q33bVal1 > 3 || q33bVal2 > 3 || q33bVal3 > 3) {
+        console.log(`q33b: Maximal drei je Spalte \n col1 ${q33bVal1} col2 ${q33bVal2}  col3 ${q33bVal3} - ${tgtName} ${evtCounter}`);
+        invalid = true;
+    }
+
+
+    if (invalid) {
+        try {
+            if (event && event.target) {
+                event.target.checked = false;
+            }                
+        } catch (error) {
+            log.error(`cannot uncheck ${event.target}`,error);
+        }
+        alert("Maximal drei je Spalte");
+    }
+
+    // console.log(`q33a col1 ${q33aVal1} col2 ${q33aVal2}  col3 ${q33aVal3}  \nq33b col1 ${q33bVal1} col2 ${q33bVal2}  col3 ${q33bVal3}`);
+
+
 
 
     return true;

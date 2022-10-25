df48bb8d222cf249f3b477e2a439d3f19e8a422b
Peter Buchmann <peter.buchmann@zew.de>
2022-10-25 14:04:04 +0200

    pds 0.06 - cx scale-x

--- app-bucket/content/pds/slider/common.js
+++ app-bucket/content/pds/slider/common.js
@@ -1,18 +1,32 @@
 
-var slider01Thumb = document.getElementById("slider01Thumb");
-slider01Thumb.value = 40; // setting an init value
+var slider01 = document.getElementById("slider01");
 
 var slider01cx1  = document.getElementsByName("slider01cx1")[0];
 var slider01cx2  = document.getElementsByName("slider01cx2")[0];
 var slider01Legend = document.getElementsByName("slider01Legend")[0];
 
 // init output windows
-// slider01cx1.value = 100 - slider01Thumb.value;
-// slider01cx2.value = slider01Thumb.value;
-slider01Legend.value = slider01Thumb.value
+// slider01cx1.value = 100 - slider01.value;
+// slider01cx2.value = slider01.value;
+// slider01Legend.value = slider01.value
+
+// slider01.value = 40; // setting an init value
+
+
+
+let initSlider = () => {
+    slider01.value = 25; 
+    // slider01.click();
+    const evt = new Event("input");
+    slider01.dispatchEvent(evt);    
+}
+
+// initSlider();
+window.addEventListener('load', initSlider, false);
+
 
 // update
-slider01Thumb.oninput = function () {
+slider01.oninput = function () {
     // slider01cx1.value = 100 - this.value;
     // slider01cx2.value = this.value;
     let incr = parseInt(this.value) + 5;

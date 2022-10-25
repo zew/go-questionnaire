b25e5b738b6a77fe35f0bf3f04d784e42ad44e61
Peter Buchmann <peter.buchmann@zew.de>
2022-10-25 11:48:02 +0200

    pds 0.05 - before absolute overlay

--- app-bucket/content/pds/slider/common.js
+++ app-bucket/content/pds/slider/common.js
@@ -4,15 +4,19 @@ slider01Thumb.value = 40; // setting an init value
 
 var slider01cx1  = document.getElementsByName("slider01cx1")[0];
 var slider01cx2  = document.getElementsByName("slider01cx2")[0];
+var slider01Legend = document.getElementsByName("slider01Legend")[0];
 
 // init output windows
-slider01cx1.value = 100 - slider01Thumb.value;
-slider01cx2.value = slider01Thumb.value;
+// slider01cx1.value = 100 - slider01Thumb.value;
+// slider01cx2.value = slider01Thumb.value;
+slider01Legend.value = slider01Thumb.value
 
 // update
 slider01Thumb.oninput = function () {
-    slider01cx1.value = 100 - this.value;
-    slider01cx2.value = this.value;
+    // slider01cx1.value = 100 - this.value;
+    // slider01cx2.value = this.value;
+    let incr = parseInt(this.value) + 5;
+    slider01Legend.value =  `${this.value}  -  ${incr}`;
 }
 
 

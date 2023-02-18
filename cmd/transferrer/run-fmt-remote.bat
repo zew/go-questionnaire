ec833f0ded1469eb614d38d449da82ed66374b1d
Peter Buchmann <peter.buchmann@zew.de>
2023-02-18 11:59:47 +0100

    transferrer configs updated

--- cmd/transferrer/run-fmt-remote.bat
+++ cmd/transferrer/run-fmt-remote.bat
@@ -8,12 +8,10 @@ cls
 setlocal
 
 @REM for execution as scheduled task
-@REM CD c:\xampp\htdocs\go-questionnaire\cmd\transferrer\
+CD c:\xampp\htdocs\go-questionnaire\cmd\transferrer\
 
 SET JOBTIME=%date:~6,4%-%date:~3,2%-%date:~0,2%-%time:~0,5%
-SET LOGFILE=import-%date:~6,4%-%date:~3,2%-%date:~0,2%.log
-@REM ECHO "JOBTIME %JOBTIME%"
-ECHO "LOGFILE %LOGFILE%"
+SET LOGFILE=fmt-import-%date:~6,4%-%date:~3,2%-%date:~0,2%.log
 
 
 @REM quotes will be in log - but I dont care
@@ -22,20 +20,17 @@ ECHO %JOBTIME%        >>%LOGFILE%
 ECHO "============="  >>%LOGFILE%
 
 
-rm ./transferrer.exe
-go build
-ECHO "built finished"
+@REM built newly or not
+@REM rm ./transferrer.exe
+@REM go build
 
-@REM standalone execution...
-@REM config dir is app-bucket
 transferrer.exe -rmt=transferrer/fmt-remote.json  >>%LOGFILE% 2>&1
-ECHO "transfer finished; see log file"
 
-@REM COPY /Y  C:\xampp\htdocs\go-questionnaire\app-bucket\responses\downloaded\fmt-*.csv C:\xampp\htdocs\fmt\Mikrodaten-ger\
+COPY /Y  C:\xampp\htdocs\go-questionnaire\app-bucket\responses\downloaded\fmt-*.csv C:\xampp\htdocs\fmt\Mikrodaten-ger\
 
 
-@REM CD C:\xampp\htdocs\fmt\
-@REM php import-fmt-from-csv.php  >>c:\xampp\htdocs\go-questionnaire\cmd\transferrer\%LOGFILE%
+CD C:\xampp\htdocs\fmt\
+php import-fmt-from-csv.php  >>c:\xampp\htdocs\go-questionnaire\cmd\transferrer\%LOGFILE%
 
 
 @REM no pause - dont stall scheduler
--- cmd/transferrer/run-pds-remote.bat
+++ cmd/transferrer/run-pds-remote.bat
@@ -1,17 +1,17 @@
 @echo off
 
 REM check admin login at remote host
-REM transferrer-endpoint?survey_id=fmt&wave_id=2021-03&fetch_all=1
+REM transferrer-endpoint?survey_id=pds&wave_id=2021-03&fetch_all=1
 cls
 
 @REM ENVIRONMENT is restored, whenever this batch exits; change directory is reset
 setlocal
 
 @REM for execution as scheduled task
-@REM CD c:\xampp\htdocs\go-questionnaire\cmd\transferrer\
+CD c:\xampp\htdocs\go-questionnaire\cmd\transferrer\
 
 SET JOBTIME=%date:~6,4%-%date:~3,2%-%date:~0,2%-%time:~0,5%
-SET LOGFILE=import-%date:~6,4%-%date:~3,2%-%date:~0,2%.log
+SET LOGFILE=pds-import-%date:~6,4%-%date:~3,2%-%date:~0,2%.log
 
 
 @REM quotes will be in log - but I dont care
@@ -20,10 +20,12 @@ ECHO %JOBTIME%        >>%LOGFILE%
 ECHO "============="  >>%LOGFILE%
 
 
-rm ./transferrer.exe
-go build
+@REM rm ./transferrer.exe
+@REM go build
 
 transferrer.exe -rmt=transferrer/pds-remote.json  >>%LOGFILE% 2>&1
 
-pause
+
+COPY /Y  C:\xampp\htdocs\go-questionnaire\app-bucket\responses\downloaded\pds-*.csv C:\xampp\htdocs\fmt\export\pcag\
+
 
